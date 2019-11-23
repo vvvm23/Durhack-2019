25053ab34d5e7028597e077c3c975b7aa284fa77
Adam Greenbank <adam.adamgreenban@gmail.com>
2019-11-23 18:38:45 +0000

    added camera controls for jenga

---
+++ Assets/cameraRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotate : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(0 , -1 * speed * Time.deltaTime, 0);
+        } 
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(0 , speed * Time.deltaTime, 0);
+        }   
+    }
+}
+
